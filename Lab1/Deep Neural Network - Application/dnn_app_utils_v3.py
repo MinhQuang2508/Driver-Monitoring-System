9096b9fa2ab56c1710155a5fa5f2c1a6bb5aee06
NghiaNT20 <nghianguyen.200303@gmail.com>
2024-02-27 20:58:55 +0700

    Update Lab

--- Lab1/Deep Neural Network - Application/dnn_app_utils_v3.py
+++ Lab1/Deep Neural Network - Application/dnn_app_utils_v3.py
@@ -85,11 +85,11 @@ def sigmoid_backward(dA, cache):
 
 
 def load_data():
-    train_dataset = h5py.File("C:\\Users\\phuon\\Desktop\\Labs\\DPL-302m\\Lab1\\Deep Neural Network - Application\\train_catvnoncat.h5", "r")
+    train_dataset = h5py.File("/Users/TrongNghia/Desktop/Lab1/Deep Neural Network - Application/train_catvnoncat.h5", "r")
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
 
-    test_dataset = h5py.File("C:\\Users\\phuon\\Desktop\\Labs\\DPL-302m\\Lab1\\Deep Neural Network - Application\\test_catvnoncat.h5", "r")
+    test_dataset = h5py.File("/Users/TrongNghia/Desktop/Lab1/Deep Neural Network - Application/test_catvnoncat.h5", "r")
     test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
--- Lab1/Logistic_Regression_with_a_Neural_Network_mindset/lr_utils.py
+++ Lab1/Logistic_Regression_with_a_Neural_Network_mindset/lr_utils.py
@@ -3,11 +3,11 @@ import h5py
     
     
 def load_dataset():
-    train_dataset = h5py.File("C:\\Users\\phuon\\Desktop\\Labs\\DPL-302m\\Lab1\\Logistic_Regression_with_a_Neural_Network_mindset\\train_catvnoncat.h5", "r")
+    train_dataset = h5py.File("/Users/TrongNghia/Desktop/Lab1/Logistic_Regression_with_a_Neural_Network_mindset/train_catvnoncat.h5", "r")
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
 
-    test_dataset = h5py.File("C:\\Users\\phuon\\Desktop\\Labs\\DPL-302m\\Lab1\\Logistic_Regression_with_a_Neural_Network_mindset\\test_catvnoncat.h5", "r")
+    test_dataset = h5py.File("/Users/TrongNghia/Desktop/Lab1/Logistic_Regression_with_a_Neural_Network_mindset/test_catvnoncat.h5", "r")
     test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
